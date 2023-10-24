f2dfd8d4f330eeb548d666bb9fc74ddf559bf25d
jskim676 <wkxndlsvk@gmail.com>
2023-10-24 13:37:56 +0900

    hi

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
+import styled from 'styled-components';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <ContentsWrapper>
+        <Header>
+          <h1>숫자 야구 게임</h1>
+        </Header>
+        <Main>
+
+
+        </Main>
+        <Footer>
+          {/* 숫자 */}
+          <div>
+            {[0,1,2,3,4,5,6,7,8,9].map(number=>{
+              return <NumerButton onClick={()=>{
+
+              }}>{number}</NumerButton>
+            })}
+          </div>
+        </Footer>
+      </ContentsWrapper>
+    </Wrapper>
   );
 }
 
 export default App;
+
+const Wrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+`;
+
+const ContentsWrapper = styled.div`
+  width: 500px;
+  height: 100%;
+  padding: 0 10px;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  border: 1px solid #cdcdcd;
+`;
+
+const Header = styled.div `
+  width:100%;
+  
+  display:flex;
+  justify-content: center;
+`
+
+const Main = styled.div `
+  width:100%;
+  height:400px;
+  border:1px solid red;
+`
+
+const Footer = styled.div `
+  width:100%;
+  height:100px;
+  border:1px solid green;
+`
+
+const NumerButton = styled.button `
+  border-radius: 5px;
+  border:1px solid blue;
+  padding:10px;
+`
